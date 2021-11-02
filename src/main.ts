86610bb5462c84ebb88a093cd86834c5e548fdd9
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2021-11-02 22:20:01 +0700

    change nodejs adapter from express to fastify

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { MainModule } from 'di/module/main.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './sharedkernel/interceptor';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MainModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    MainModule,
+    new FastifyAdapter(),
+  );
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3000);
+  await app.listen(3000, '127.0.0.1');
 }
 
 bootstrap();
