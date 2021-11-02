1c352b6bcb22d2bfafcdcf2bc97f66a75acf78f6
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2021-11-02 22:35:02 +0700

    setting basic of swagger and CSP config

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { fastifyHelmet } from 'fastify-helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -14,6 +16,27 @@ async function bootstrap() {
   );
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJs Swagger Example')
+    .setDescription('The nestjs swagger API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.register(fastifyHelmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [`'self'`],
+        styleSrc: [`'self'`, `'unsafe-inline'`],
+        imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
+        scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+      },
+    },
+  });
+
   await app.listen(3000, '127.0.0.1');
 }
 
